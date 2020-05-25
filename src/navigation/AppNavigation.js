f833e6f22bb6fa1fe13f29d08b9396d24e9d7043
Alejandro Gonzalez S <mgonzalez@teravisiontech.com>
2020-05-25 01:50:34 -0500

    Add icons to menu tabs

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,18 +3,48 @@ import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Feed from '../views/Feed';
+import Icon from 'react-native-vector-icons/Feather';
+import Avatar from '../components/Avatar';
 
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+const componentText = (name = 'Instagram') => () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>{name}</Text>
+  </View>
+);
 
 const tabs = [
-  {name: 'Feed', component: Feed},
-  {name: 'Settings', component: SettingsScreen},
+  {
+    name: 'Feed',
+    component: Feed,
+    icon: <Icon name="home" size={30} color="#1c1c1c" />,
+  },
+  {
+    name: 'Search',
+    component: componentText('Search'),
+    icon: <Icon name="search" size={30} color="#1c1c1c" />,
+  },
+  {
+    name: 'Add Photo',
+    component: componentText('Add photo'),
+    icon: <Icon name="plus-square" size={30} color="#1c1c1c" />,
+  },
+  {
+    name: 'Likes',
+    component: componentText('Likes'),
+    icon: <Icon name="heart" size={30} color="#1c1c1c" />,
+  },
+  {
+    name: 'Profile',
+    component: componentText('Profile'),
+    icon: (
+      <Avatar
+        size={35}
+        image={
+          'https://elpais.com/cultura/imagenes/2016/08/01/television/1470053691_755707_1470053945_noticia_fotograma.jpg'
+        }
+      />
+    ),
+  },
 ];
 
 const Tab = createBottomTabNavigator();
@@ -22,7 +52,16 @@ const Tab = createBottomTabNavigator();
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        style={{paddingTop: 10}}
+        labeled={false}
+        tabBarOptions={{
+          showLabel: false,
+        }}
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) =>
+            tabs.find(x => x.name === route.name).icon,
+        })}>
         {tabs.map(({name, component}, index) => (
           <Tab.Screen key={index} name={name} component={component} />
         ))}
